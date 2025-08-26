d6f9c089f2e16025f31907758bad4a511bdb6a7f
Sreedeviraj <sreedeviraj26@gmail.com>
2025-08-26 15:42:14 +0530

    requirement_txt

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,16 @@
 
 
-import motor.motor_asyncio
+# import motor.motor_asyncio
 
-# MongoDB Atlas connection
-MONGO_DETAILS = "mongodb+srv://sreedeviraj:sree123@cluster0.oic5wy1.mongodb.net/parkease?retryWrites=true&w=majority&appName=Cluster0"
+# # MongoDB Atlas connection
+# MONGO_DETAILS = "mongodb+srv://sreedeviraj:sree123@cluster0.oic5wy1.mongodb.net/parkease?retryWrites=true&w=majority&appName=Cluster0"
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
-db = client["add2video"]   # expose db
-ads_collection = db["ads"] 
+# client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+# db = client["add2video"]   # expose db
+# ads_collection = db["ads"] 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_DETAILS = os.getenv("MONGODB_URI", "mongodb+srv://sreedeviraj:sree123@cluster0.oic5wy1.mongodb.net/parkease?retryWrites=true&w=majority&appName=Cluster0")
